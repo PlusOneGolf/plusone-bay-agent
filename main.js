3fcd6cbc6218073ab965b6a8ef62daf6a04015d8
PlusOneGolf <tylerkersten@gmail.com>
2026-02-23 16:15:16 -0600

    Fix timer HUD: use opaque dark window instead of transparent (Windows compat)

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const { app, BrowserWindow, globalShortcut, ipcMain, screen } = require("electron");
 const path = require("path");
 
+app.disableHardwareAcceleration();
+
 let win;
 let timerWin;
 let isOverlayVisible = true;
 let isTimerVisible = false;
+let timerReady = false;
 
 function createWindow() {
   win = new BrowserWindow({
@@ -45,25 +48,23 @@ function createWindow() {
 function createTimerWindow() {
   const display = screen.getPrimaryDisplay();
   const screenWidth = display.workAreaSize.width;
-  const hudWidth = 220;
-  const hudHeight = 50;
+  const hudWidth = 240;
+  const hudHeight = 48;
   const xPos = Math.round((screenWidth - hudWidth) / 2);
 
   timerWin = new BrowserWindow({
     width: hudWidth,
     height: hudHeight,
     x: xPos,
-    y: 12,
+    y: 0,
     frame: false,
-    transparent: true,
-    backgroundColor: "#00000000",
     alwaysOnTop: true,
     skipTaskbar: true,
     focusable: false,
     resizable: false,
     movable: false,
     hasShadow: false,
-    thickFrame: false,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -74,11 +75,25 @@ function createTimerWindow() {
   timerWin.setIgnoreMouseEvents(true);
   timerWin.setVisibleOnAllWorkspaces(true);
   timerWin.loadFile(path.join(__dirname, "timer-hud.html"));
-  timerWin.hide();
 
   timerWin.webContents.on("did-finish-load", () => {
+    timerReady = true;
     timerWin.webContents.setBackgroundThrottling(false);
   });
+}
+
+function showTimer() {
+  if (timerWin && !isTimerVisible && timerReady) {
+    isTimerVisible = true;
+    timerWin.showInactive();
+  }
+}
+
+function hideTimer() {
+  if (timerWin && isTimerVisible) {
+    isTimerVisible = false;
+    timerWin.hide();
+  }
 }
 
 ipcMain.on("overlay:show", () => {
@@ -105,17 +120,11 @@ ipcMain.on("overlay:hide", () => {
 });
 
 ipcMain.on("timer:show", () => {
-  if (timerWin && !isTimerVisible) {
-    isTimerVisible = true;
-    timerWin.show();
-  }
+  showTimer();
 });
 
 ipcMain.on("timer:hide", () => {
-  if (timerWin && isTimerVisible) {
-    isTimerVisible = false;
-    timerWin.hide();
-  }
+  hideTimer();
 });
 
 ipcMain.on("timer:update", (event, data) => {
